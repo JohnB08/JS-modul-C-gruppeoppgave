1e74fb75a9662881cb96de7a9ddb8fb943bf1eb9
JohnB08 <johnb08.kodehode@gmail.com>
2023-11-14 09:16:24 +0100

    Adda beskrivelser for import/export

--- index.mjs
+++ index.mjs
@@ -1,1 +1,10 @@
-import { makeElements } from "./jsModules/makeElementsFunction";
+/* !!IMPORT MÅ STÅ ØVERST!! */
+
+//importerer makeElements funksjonen fra makeElemenentsFunction modulen. Da kan jeg bruke den her.
+import { makeElements } from "./jsModules/makeElementsFunction.js";
+
+let testBtn = makeElements("button", {
+  className: "testBtn",
+  textContent: "testBtn",
+});
+document.body.appendChild(testBtn);
--- jsModules/makeElementsFunction.js
+++ jsModules/makeElementsFunction.js
@@ -1,4 +1,6 @@
-//funksjon for å lage html elementer basert på type, og et object med key/value par
+//Når vi lager en modul, tror jeg det er lurt på bruke JS docs for å gi en beskrivelse.
+
+/**funksjon for å lage html elementer basert på type, og et object med key/value par*/
 function makeElements(type, properties) {
   const propertyArray = Object.entries(properties);
   const element = document.createElement(type);
@@ -10,4 +12,6 @@ function makeElements(type, properties) {
 }
 
 //i bunn eksporterer funksjonen ut av dokumentet.
+//du kan importere denne inn i en ny JS modul via import
+/* !!EXPORT MÅ STÅ I  BUNN!! */
 export { makeElements };
